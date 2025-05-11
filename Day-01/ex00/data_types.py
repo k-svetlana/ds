224add88942a9e0f65266905137f3ee99d3d01e5
Svetlana <112399684+k-svetlana@users.noreply.github.com>
2025-05-11 19:54:43 +0300

    Update data_types.py

--- Day-01/ex00/data_types.py
+++ Day-01/ex00/data_types.py
@@ -1,25 +1,26 @@
-#!/usr/bin/env python3
-
 def data_types():
-    a = 42
-    b = "hey"
-    c = 3.14
-    d = True
-    e = ["apple", "banana", "cherry"]
-    f = {"type": "dictionary"}
-    g = ("red", "green", "blue")
-    h = {7, 8, 9}
-    
-    types_list = [
-        type(a).__name__,
-        type(b).__name__,
-        type(c).__name__,
-        type(d).__name__,
-        type(e).__name__,
-        type(f).__name__,
-        type(g).__name__,
-        type(h).__name__]
-    print(types_list)
+    num = 42
+    word = 'hey'
+    float_num = 3.14
+    bool_var = True
+    fruit_list = ['apple','banana','cherry']
+    dict_var = {'type':'dictionary'}
+    colors_tuple = ('red','green','blue')
+    num_set = {7, 8, 9}
 
-if __name__ == "__main__":
+
+    types_list = [
+        type(num).__name__,
+        type(word).__name__,
+        type(float_num).__name__,
+        type(bool_var).__name__,
+        type(fruit_list).__name__,
+        type(dict_var).__name__,
+        type(colors_tuple).__name__,
+        type(num_set).__name__]
+    print('[', end='')
+    print(*types_list, sep=', ', end='')
+    print(']')
+
+if __name__ == '__main__':
     data_types()
